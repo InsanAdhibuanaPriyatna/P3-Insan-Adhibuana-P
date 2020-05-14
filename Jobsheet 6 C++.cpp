4c080175879509bb984b1fec3fa890db25ef034b
InsanAdhibuanaPriyatna <64582598+InsanAdhibuanaPriyatna@users.noreply.github.com>
2020-05-13 19:51:58 -0700

    Create Jobsheet 6 C++.cpp
    Fungsi tanpa nilai balik- 
    Buatlah program menampilkan angka 9 s/d 0 dengan perulangan.

---
+++ Jobsheet 6 C++.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include <math.h>
+#include<stdio.h>
+
+using namespace std;
+
+void contoh(){
+for(int i = 9;i>=0;i--){
+ cout<<"\nAngka "<<i;
+ }
+} 
+
+int main(){
+contoh();
+}
