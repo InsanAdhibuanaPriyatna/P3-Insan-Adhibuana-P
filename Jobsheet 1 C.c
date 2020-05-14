60f0aed3b3bb71bcef7a13ac703f03bbd9243c5f
InsanAdhibuanaPriyatna <64582598+InsanAdhibuanaPriyatna@users.noreply.github.com>
2020-05-13 19:42:32 -0700

    Create Jobsheet 1 C.c
    Struktur dasar Fungsi – Buatlah fungsi untuk menampilkan penjumlahan tgl dan bulan lahir

---
+++ Jobsheet 1 C.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+using namespace std;
+
+int tgl,bulan;
+void penjumlahan(){
+ tgl = 18;
+ bulan = 3;
+ int l;
+ l=tgl+bulan;
+ printf("hasil = %i ",l);
+ }	
+
+ int main() 
+{
+ penjumlahan();
+ }
