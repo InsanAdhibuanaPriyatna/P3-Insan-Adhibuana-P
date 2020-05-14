5c50beb46bcafa40237623699de21d15f7921c06
InsanAdhibuanaPriyatna <64582598+InsanAdhibuanaPriyatna@users.noreply.github.com>
2020-05-13 19:45:36 -0700

    Create Jobsheet 2 C++.cpp
    Deklarasi Fungsi dan Pemanggilan fungsi – Buatlah sebuah fungsi untuk menampilkan bilangan genap dari nol sampai dua nim trakhir anda.

---
+++ Jobsheet 2 C++.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int contoh();
+
+int main(){
+contoh(); 
+  }	
+  
+int contoh(){
+cout<<"Bilangan Ganjil : "<<endl;
+for(int i=0;i<=73;i++){
+ if(i%2 == 0){ 
+cout<<" "<<i; 
+    }
+  } 
+}
